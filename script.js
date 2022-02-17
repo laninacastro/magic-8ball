7d0b09392dc8b56e7016b973b8d7082ae2f23394
Belinda <90870640+laninacastro@users.noreply.github.com>
2022-02-17 17:07:58 -0500

    final JS

--- script.js
+++ script.js
@@ -1,27 +1,33 @@
-// Create a div with an id of "affirmations" that you will insert the affirmations into using js
 
-const myAffirmations = document.getElementById('affirmations');  {
-    let innerHTML = `
+const affirmationsArray = ['You\'re hella worthy.', 'Share your light with someone new today', 'You are in your own lane. Don\'t compare yourself to others', 'I\'m so proud of you', 'Your skin is glistening.', 'Your ability to conquer challenges is limitless.', 'Overcome your fears and follow your dreams.', 'Feed your spirit. Train your body. Focus your mind. It\'s your time.', 'You will learn Javascript!', 'No one belongs here more than you.']
+
+const myAffirmations = document.getElementById('affirmations');  
+     myAffirmations.innerHTML = `
         <div>
             <p>    
 
             </p>
         </div>
     `
+    //Generate random #
+function randomOneOf(){
+   var numberLimit=affirmationsArray.length 
+    var randomNumber=Math.random() * 100
+    var randomIndex=Math.floor(randomNumber)%numberLimit
+    return affirmationsArray[randomIndex]
 }
-debugger
 
-// Create an "ask" function that when clicked, will choose a random string and insert it into your "affirmations" div
-
- 
+    
     function ask() {
-
-        affirmationsArray= RandomOneOf(['You\'re hella worthy.', 'Share your light with someone new today', 'You are in your own lane. Don\'t compare yourself to others', 'I\'m so proud of you', 'Your skin is glistening.', 'Your ability to conquer challenges is limitless.', 'Overcome your fears and follow your dreams.', 'Feed your spirit. Train your body. Focus your mind. It\'s your time.', 'You will learn Javascript!', 'No one belongs here more than you.']);
-
-        document.getElementById('askBtn').onclick = function ask();
-
-        document.getElementById('affirmations'),innerHTML = 
-            + affirmationsArray +
+        myAffirmations.innerHTML = `
+        <div>
+            <p>    
+            ${randomOneOf()}
+            </p>
+        </div>
+        `
     }
+
+    document.getElementById('flowerBtn').onclick = ask;
 
 
